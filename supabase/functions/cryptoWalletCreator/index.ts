0c37708b65182e291bda1b5591411001a3f90427
Alasdair <afullerca@gmail.com>
2024-03-14 00:08:39 -0700

    refactored code for cryptoWallerCreator function

--- supabase/functions/cryptoWalletCreator/index.ts
+++ supabase/functions/cryptoWalletCreator/index.ts
@@ -1,82 +1,78 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js'
-import {Web3} from 'https://esm.sh/web3'
+    import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+    import {Web3} from 'https://esm.sh/web3'
 
-// create wallet for a new user if no wallet exists
-Deno.serve(async (_req) =>{
+    const web3 = new Web3()
 
-// Parse the URL and query parameters
-const web3 = new Web3()
-const url = new URL(_req.url)
+    async function addWalletToUserIfNotExist(request) {
 
-const userId = url.searchParams.get('userId') 
-const wallet = web3.eth.accounts.create()
-const privateKey = wallet.privateKey
-const publicKey = wallet.address
+        const url = new URL(request.url)
+        const userId = url.searchParams.get('userId') 
+        const wallet = web3.eth.accounts.create()
+        const privateKey = wallet.privateKey
+        const publicKey = wallet.address
 
-
-// check if user exists
-// check if user has a wallet 
-// if no wallet exists create a new wallet
-// update user's private and public keys in the database
-async function addWalletToNewUser(userId, privateKey, publicKey) {
-
-    try {
         const supabase = createClient(
-          Deno.env.get('SUPABASE_URL') ?? '',
-          Deno.env.get('SUPABASE_ANON_KEY') ?? '',
-          { global: { headers: { Authorization: _req.headers.get('Authorization')! } } }
+            Deno.env.get('SUPABASE_URL') ?? '',
+            Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+            { global: { headers: { Authorization: request.headers.get('Authorization')! } } }
         )
 
-    const { data: userData, error: fetchError } = await supabase
+        try {
+            const userData = await fetchUser(supabase, userId)
+            if (!userData){
+                console.log('No user found or user already has a wallet')
+                return new Response(JSON.stringify({error: 'No user found or user already has a wallet'}), {status: 404})
+            }
+            
+            await updateUserKeys(supabase, userId, privateKey, publicKey)
+            console.log('User keys updated successfully:')
+
+            return new Response(JSON.stringify({privateKey, publicKey}), {status: 202, headers: {'Content-Type': 'application/json'}})
+
+        }catch (error){
+            console.error('Error:', error)
+            return new Response(JSON.stringify({error: error.message}), {status: 500, headers: {'Content-Type': 'application/json'}})
+        }    
+    }
+
+    async function fetchUser(supabase, userId){
+        
+        const { data: userData, error: fetchError } = await supabase
         .from('users')
         .select('private_key, public_key')
         .eq('user_id', userId)
         .is('private_key', null)
         .is('public_key', null)
-        .single()
+        .maybeSingle()
 
-    if (fetchError){
-        console.error("Error fetching user: ", fetchError)
-        return
+        if (fetchError) {
+            console.error('Operational error fetching user:', fetchError);
+            throw new Error('Error fetching user data due to operational issue');
+        }
+        return userData
     }
 
-    if (!userData){
-        console.error("Error fetching user:", fetchError)
-        return
+    async function updateUserKeys(supabase, userId, privateKey, publicKey) {
+
+        const {error: updateError} = await supabase
+            .from('users')
+            .update({
+                private_key: privateKey,
+                public_key: publicKey
+            })
+            .eq('user_id', userId)
+
+        if (updateError) {
+                throw new Error('Error updating user keys')
+        }   
     }
 
-    //Add wallets to user
-    const {data: updateData, error: updateError} = await supabase
-        .from('users')
-        .update({
-            private_key: privateKey,
-            public_key: publicKey
-         })
-        .eq('user_id', userId)
 
-    if (updateError) {
-        console.error('Error updating user keys: ', updateError)
-        return
-    }
+    Deno.serve(async (request) =>{
 
-    console.log('User keys updated successfully:', updateData);
+        return addWalletToUserIfNotExist(request)
+        
+    })
 
-    
-    } catch (err) {
-     // return new Response(String(err?.message ?? err), { status: 500 })
-    }
-    
-}
-
-    addWalletToNewUser(userId, privateKey, publicKey)
-
-    return new Response(JSON.stringify({ wallet }), {
-        headers: { 'Content-Type': 'application/json' },
-        status: 200,
-      })
-    
-})
-
-// mine new tokens and add the equivalent of 20 free tokens to a wallet
 
 
